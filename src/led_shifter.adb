593b6fb76779bb989b273cf4fe5db7841e3833d6
Luke A. Guest <laguest@archeia.com>
2016-11-27 19:54:14 +0000

    Initial first blinky lights test.

---
+++ src/led_shifter.adb
@@ -0,0 +1,32 @@
+with AVR; use AVR;
+with AVR.MCU;
+with AVR.Real_Time.Clock;
+
+procedure LED_Shifter is
+begin
+   --  Set the Digital pins 0 and 13 as output.
+   MCU.DDRD_Bits (MCU.DDD0_Bit) := DD_Output;
+   MCU.DDRB_Bits (MCU.DDD5_Bit) := DD_Output;
+
+   --  Turn on the LED on pin 0 and the LED on pin 13 off.
+   MCU.PORTD_Bits (MCU.PORTD0_Bit) := High;
+   MCU.PORTB_Bits (MCU.PORTB5_Bit) := Low;
+
+   delay 1.0;
+
+   --  Switch the LEDs.
+   MCU.PORTD_Bits (MCU.PORTD0_Bit) := Low;
+   MCU.PORTB_Bits (MCU.PORTB5_Bit) := High;
+
+   delay 2.0;
+
+   --  Switch the LEDs.
+   MCU.PORTD_Bits (MCU.PORTD0_Bit) := High;
+   MCU.PORTB_Bits (MCU.PORTB5_Bit) := Low;
+
+   delay 1.0;
+
+   --  Switch the LEDs.
+   MCU.PORTD_Bits (MCU.PORTD0_Bit) := Low;
+   MCU.PORTB_Bits (MCU.PORTB5_Bit) := High;
+end LED_Shifter;
